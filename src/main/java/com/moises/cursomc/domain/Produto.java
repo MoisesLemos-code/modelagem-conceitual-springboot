67d84eeda21b08b40f9953b401446a4b54b63f09
MoisesLemos-code <moises_lemos@outlook.com>
2020-01-06 00:03:31 -0300

    Endpoint /pedido/{id} disponibilizado

--- src/main/java/com/moises/cursomc/domain/Produto.java
+++ src/main/java/com/moises/cursomc/domain/Produto.java
@@ -17,6 +17,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="PRODUTO")
@@ -38,6 +39,7 @@ public class Produto implements Serializable{
 			)
 	private List<Categoria> categorias = new ArrayList<>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy="id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 	
@@ -56,6 +58,7 @@ public class Produto implements Serializable{
 		return id;
 	}
 
+	@JsonIgnore
 	public List<Pedido> getPedidos(){
 		List<Pedido> lista = new ArrayList<>();
 		for(ItemPedido x : itens) {
---
+++ src/main/java/com/moises/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.moises.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moises.cursomc.domain.Pedido;
+import com.moises.cursomc.repositories.PedidoRepository;
+import com.moises.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired //Instanciar automaticamente.
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: "+ Pedido.class.getName());
+		}
+		return obj;
+	}
+}
