daae915d6056a7c3df26ed3da3bfc990bec02394
MoisesLemos-code <moises_lemos@outlook.com>
2020-01-20 20:57:21 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/moises/cursomc/services/CategoriaService.java
+++ src/main/java/com/moises/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.moises.cursomc.domain.Categoria;
+import com.moises.cursomc.dto.CategoriaDTO;
 import com.moises.cursomc.repositories.CategoriaRepository;
 import com.moises.cursomc.services.exceptions.DataIntegrityException;
 import com.moises.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome() );
 	}
 }
