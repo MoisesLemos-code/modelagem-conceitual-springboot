0d00ded57083d5fe47ab25ee4075e7c5c5a71b2b
MoisesLemos-code <moises_lemos@outlook.com>
2020-01-20 21:49:47 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/moises/cursomc/services/ClienteService.java
+++ src/main/java/com/moises/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.moises.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.moises.cursomc.domain.Cliente;
+import com.moises.cursomc.dto.ClienteDTO;
 import com.moises.cursomc.repositories.ClienteRepository;
+import com.moises.cursomc.services.exceptions.DataIntegrityException;
 import com.moises.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,5 +28,42 @@ public class ClienteService {
 					+ ", Tipo: "+ Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.delete(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um registro com referências!");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	//Paginação
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//Atualizar somente nome e email de cliente
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	//Atualizar somente nome e email de cliente
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
